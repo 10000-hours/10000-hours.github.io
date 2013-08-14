ec5e9163827b7e41bf9d32020ad2d45707cc0f44
Zhu Tao <zhutao.iscas@gmail.com>
2013-08-14 20:20:27 +0800

    migrate from towerjoo.github.com(only tech part)

---
+++ gen.py
@@ -0,0 +1,39 @@
+#coding:utf-8
+skeleton = """---
+layout: post
+title: %(title)s
+category: %(category)s
+tags: 
+---"""
+from optparse import OptionParser
+import os, sys
+from datetime import datetime
+
+parser = OptionParser()
+parser.add_option("-t", "--title", dest="title", help="Title of the post")
+parser.add_option("-c", "--category", dest="category", help="category of the post")
+parser.add_option("-n", "--name", dest="name", help="name of the post")
+
+(options, args) = parser.parse_args()
+if options.category and options.name:
+    today = datetime.today().strftime("%Y-%m-%d")
+    post_name = "%s-%s.md" % (today, options.name)
+    post_name = os.path.join("_posts", post_name)
+    if os.path.exists(post_name):
+        print "%s already exists!" % post_name
+        parser.print_help()
+        sys.exit(0)
+    else:
+        title = options.title if options.title else ""
+        cont = skeleton % {"title" : title, "category" : options.category}
+        fh = open(post_name, "w")
+        fh.write(cont)
+        fh.close()
+        print "%s created successfully!" % post_name
+        sys.exit(0)
+else:
+    print "You must specify category of the post, and also the name of the file"
+    parser.print_help()
+    sys.exit(0)
+
+
---
+++ links.markdown
@@ -0,0 +1,12 @@
+---
+layout: list
+title: Links
+---
+
+{% include header.tpl %}
+<div id="main">
+
+# Links
+
+To be added.
+</div>
